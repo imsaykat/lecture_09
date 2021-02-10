a36f7873c3a100645852ec4ddede62d3e1884b40
“PROGRAMMER_SAYKAT” <saykatghosh79.gmail.com>
2021-02-10 20:54:29 +0600

    Primitive data type

---
+++ main_09.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+//var name string
+//var age int
+//var result float32
+//var char rune
+//var isFound bool
+
+var chr rune
+chr = 'b'
+fmt.Println(chr)
+fmt.Printf("%c",chr)
+
+/*
+  fmt.Println(name)
+  fmt.Println(age)
+  fmt.Println(result)
+  fmt.Println(char)
+  fmt.Println(isFound)
+  fmt.Println(true && true)
+  fmt.Println(true && false)
+  fmt.Println(true || true)
+  fmt.Println(true || false)
+  fmt.Println(!true)*/
+}
